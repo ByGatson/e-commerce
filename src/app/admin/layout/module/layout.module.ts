c9147874f36232ca5660b6ccf3f50471eef39a77
BYGATSON\bayja <bayjava18@gmail.com>
2025-05-11 16:07:52 +0300

    first version 1105

---
+++ src/app/admin/layout/module/layout.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { ComponentsModule } from '../components/module/components.module';
+import { LayoutComponent } from '../layout.component';
+
+@NgModule({
+  declarations: [LayoutComponent],
+  imports: [CommonModule, LayoutRoutingModule, ComponentsModule],
+  exports: [LayoutComponent],
+})
+export class LayoutModule {}
